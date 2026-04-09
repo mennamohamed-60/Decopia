85dfb9f5925e7accffca40a43055a2ba95243ef8
mennamohamed-60 <sabahmostafa465@gmail.com>
2026-04-10 01:07:50 +0200

    update home navbar

--- src/Components/HomeNavbar/HomeNavbar.jsx
+++ src/Components/HomeNavbar/HomeNavbar.jsx
@@ -26,7 +26,7 @@ export default function HomeNavbar() {
           </Link>
 
           {/* Buttons */}
-          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <div className="flex md:order-2">
             <button
               onClick={() => navigate("/login")}
               type="button"
@@ -37,30 +37,7 @@ export default function HomeNavbar() {
 
 
             
-            <button
-              data-collapse-toggle="navbar-cta"
-              type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-300 rounded-lg md:hidden hover:bg-slate-800 focus:outline-none focus:ring-2 focus:ring-gray-600"
-              aria-controls="navbar-cta"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
+           
           </div>
 
           {/* Navbar links */}
